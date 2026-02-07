24d6d8f8aee3e014691f5b7fd22516b5ece629ce
Caio do Carmo <caio.carmo0521@gmail.com>
2026-02-07 13:14:25 -0300

    Reponsividade adicionada no index principal

--- docs/Sorteador/scripts/main.js
+++ docs/Sorteador/scripts/main.js
@@ -11,9 +11,50 @@ const myButton = document.querySelector('button')
 function sortearNumero() {
 
     // Pegando os valores Digitados
-    const inputMin = Number(document.querySelector('.inputMin').value)
-    const inputMax = Number(document.querySelector('.inputMax').value)
+    const inputMinValue = document.querySelector('.inputMin').value
+    const inputMaxValue = document.querySelector('.inputMax').value
 
+
+    // Verifica se à valor no input
+    if (inputMinValue == "" || inputMaxValue == "") {
+
+        alert('Você tem que estipular um valor MÍNIMO e um valor MÁXIMO!')
+
+        const textoPlaceholder = document.querySelector('.textoFlutuante')
+        const textoDoResultado = document.querySelector('.textoNumeroSorteado')
+        const valorSorteadoExibido = document.querySelector('.numeroSorteado')
+
+        // limpa a tela
+        textoDoResultado.style.display = 'none'
+        valorSorteadoExibido.style.display = 'none'
+        textoPlaceholder.style.display = 'block'
+        return;
+    } else if (inputMinValue > inputMaxValue) {
+        alert('O valor MÍNIMO não pode ser maior que o valor MÁXIMO')
+
+        const textoPlaceholder = document.querySelector('.textoFlutuante')
+        const textoDoResultado = document.querySelector('.textoNumeroSorteado')
+        const valorSorteadoExibido = document.querySelector('.numeroSorteado')
+
+        // limpa a tela
+        textoDoResultado.style.display = 'none'
+        valorSorteadoExibido.style.display = 'none'
+        textoPlaceholder.style.display = 'block'
+        return;
+
+    } else if (inputMinValue == inputMaxValue) {
+        alert('O valor MÍNIMO e o valor MÁXIMO não podem ser iguais')
+
+        const textoPlaceholder = document.querySelector('.textoFlutuante')
+        const textoDoResultado = document.querySelector('.textoNumeroSorteado')
+        const valorSorteadoExibido = document.querySelector('.numeroSorteado')
+
+        // limpa a tela
+        textoDoResultado.style.display = 'none'
+        valorSorteadoExibido.style.display = 'none'
+        textoPlaceholder.style.display = 'block'
+        return;
+    }
 
     // Adicionando o texto 'Número sorteado'
     const textoDoResultado = document.querySelector('.textoNumeroSorteado')
@@ -23,6 +64,10 @@ function sortearNumero() {
     const textoPlaceholder = document.querySelector('.textoFlutuante')
     textoPlaceholder.style.display = 'none'
 
+    // Converte o valor digitado no INPUT pra number 
+    const inputMin = Number(inputMinValue);
+    const inputMax = Number(inputMaxValue);
+
     //Fazendo o calculo pra vir os numeros colocado no input
     const numberRandom = Math.floor(Math.random() * (inputMax - inputMin + 1)) + inputMin;
 
@@ -30,20 +75,6 @@ function sortearNumero() {
     const valorSorteadoExibido = document.querySelector('.numeroSorteado')
     valorSorteadoExibido.style.display = 'block'
     valorSorteadoExibido.innerHTML = numberRandom
-
-    // Verifica se a valor no input
-    if (inputMin == "" || inputMax == "") {
-
-        alert('Você tem que estipular um valor MÍNIMO e um valor MÁXIMO!')
-
-        // limpa a tela
-        textoDoResultado.style.display = 'none'
-        valorSorteadoExibido.style.display = 'none'
-
-        textoPlaceholder.style.display = 'block'
-        return;
-    }
-
 }
 
 // Colocando um evento quanto o botão for clicado, chama a função.
